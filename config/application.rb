0ebb032ea498cf7b1d869b9db42762eb7f4603a8
cedric <cedric@studio-sept.be>
2014-02-06 08:59:26 +0100

    Angular : WIP

--- config/application.rb
+++ config/application.rb
@@ -19,11 +19,6 @@ module CommunityGreenlabCoworking
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :fr
-    if defined? ::HamlCoffeeAssets
-      config.hamlcoffee.name_filter = lambda { |n| n.sub /^backbone\/templates\//, '' }
-      config.hamlcoffee.escapeHtml = false
-    end
-
 
   end
 end
