f8ce562338ce932d8018f561150ff4d19f51dfcd
cedric <cedric@studio-sept.be>
2014-02-22 16:27:52 +0100

    hashtag bug fix

--- lib/community/simple_hashtag/hashtag.rb
+++ lib/community/simple_hashtag/hashtag.rb
@@ -1,6 +1,6 @@
 module SimpleHashtag
   class Hashtag < ActiveRecord::Base
-    HASHTAG_REGEX = /(|^)(#(?!(?:\d+|\w+?_|_\w+?)(?:\s|$))([a-z0-9\-_]+))/i
+    HASHTAG_REGEX = /(?:)(#(?!(?:\d+|\w+?_|_\w+?)(?:\s|$))([a-z0-9\-_]+))/i
 
     def count
       @count ||= 0
--- spec/helpers/hashtags_helpers_spec.rb
+++ spec/helpers/hashtags_helpers_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe HashtagsHelper do
   it "Transforms hashtags into links" do
-    expect(helper.linkify_hashtags("foo #bar")).to eq("foo <a class=\"hashtag\" href=\"#{hashtag_path('#bar')}\">#bar</a>")
+    expect(helper.linkify_hashtags("foo #bar")).to eq("foo <a class=\"hashtag\" href=\"#{hashtag_path('bar')}\">#bar</a>")
+    expect(helper.linkify_hashtags("This is a #message with a #hashtag")).to eq("This is a <a class=\"hashtag\" href=\"#{hashtag_path('message')}\">#message</a> with a <a class=\"hashtag\" href=\"#{hashtag_path('hashtag')}\">#hashtag</a>")
   end
 end
