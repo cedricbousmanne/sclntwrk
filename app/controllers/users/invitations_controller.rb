fbfa304f0778e0c410f53ebddefd7eb735221bad
cedric <cedric@studio-sept.be>
2014-03-12 14:27:28 +0100

    Bug fix: do not re-invite members

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -3,10 +3,15 @@ class Users::InvitationsController < Devise::InvitationsController
   before_filter :authorize_invitation
 
   def create
-    invite_resource.communities << current_community
-    invite_resource.save
-    invite_resource.create_activity key: 'user.invited_by', owner: current_user, community_id: current_community.id
-    redirect_to root_path, notice: "Un e-mail d'invation a été envoyé à #{invite_resource.email}"
+    unless invite_resource.communities.include?(current_community)
+      invite_resource.communities << current_community
+      invite_resource.create_activity key: 'user.invited_by', owner: current_user, community_id: current_community.id
+      invite_resource.save
+      notice = "Un e-mail d'invation a été envoyé à #{invite_resource.email}"
+    else
+      notice = "#{invite_resource.email} fait déjà partie de la communauté"
+    end
+    redirect_to root_path, notice: notice
   end
 
   private
