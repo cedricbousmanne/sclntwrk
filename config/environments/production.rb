ea554c2eb1a4d7d7b7e72d5f0e0bb7594cf507e6
cedric <cedric@studio-sept.be>
2014-03-12 10:30:35 +0100

    asset_host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,9 @@ CommunityGreenlabCoworking::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = Proc.new { |source|
+    (source  =~ /\/(pdf)|(media)\//).nil? ? "https://#{CONFIG[:aws][:asset_host]}" : nil
+  }
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
