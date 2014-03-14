492f995dd249372d01aa30d60f02036244b9ad95
cedric <cedric@studio-sept.be>
2014-03-14 11:14:37 +0100

    Un utilisateur à une langue (FR)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_community
   include Concerns::Mailers
-  before_action :set_mailer_config
+  before_action :set_locale, :set_mailer_config
   layout :layout_by_resource
 
   private
+
+  def set_locale
+    locale      = :fr
+    locale      = current_user.locale if current_user
+    I18n.locale = locale
+  end
 
   def current_community
     @current_community ||= Community.find_by_domain(request.host)
